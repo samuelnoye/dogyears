f10a474a5253f36208d6d37cdd6aa73abfbb9df6
Noye Samuel <samule.noye@amalitech.org>
2021-07-19 12:31:17 +0000

    initial commit

---
+++ dogyears.js
@@ -0,0 +1,22 @@
+//create and assign value to age
+let myAge = 30
+    //create and assign value to early years
+let earlyYears = 2
+    //multiply early years
+earlyYears *= 10.5
+    //create and assign value to later years
+let laterYears = myAge - 2
+    //multiply early lateryears
+laterYears *= 4
+    //print both early years and later years
+console.log(earlyYears)
+console.log(laterYears)
+    //declare and assign value to myAgeInDogYears
+let myAgeInDogYears = earlyYears + laterYears
+    //Write your name as a string
+let myName = 'NOYE'
+    //convert name to lowercase
+myName.toLowerCase() g
+    //print name in lowercase
+console.log(myName.toLowerCase())
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
